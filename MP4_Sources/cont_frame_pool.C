ca10fdaa9fcb84cbf00df5a17784e9c5d9cf7ce3
Joseph Renteria <josephrent@tamu.edu>
2020-10-16 14:59:55 -0500

    MP4 code added

--- MP4_Sources/cont_frame_pool.C
+++ MP4_Sources/cont_frame_pool.C
@@ -93,7 +93,7 @@
 /*--------------------------------------------------------------------------*/
 /* DEFINES */
 /*--------------------------------------------------------------------------*/
-
+#define KB * (0x1 << 10)
 /* -- (none) -- */
 
 /*--------------------------------------------------------------------------*/
@@ -132,31 +132,342 @@ ContFramePool::ContFramePool(unsigned long _base_frame_no,
                              unsigned long _info_frame_no,
                              unsigned long _n_info_frames)
 {
-    // TODO: IMPLEMENTATION NEEEDED!
-    assert(false);
+    base_frame_no = _base_frame_no;
+    nframes = _nframes;
+    n_free_frames = _nframes;
+    info_frame_no = _info_frame_no;
+    n_info_frames = _n_info_frames;
+
+    // If _info_frame_no is zero then we keep management info in the first
+    // frame, else we use the provided frame(s) to keep management info
+    if (info_frame_no == 0) {
+        // Bitmap must fit in a single frame, each frame needs 2 bits
+        assert(_nframes * 2 <= FRAME_SIZE * 8);
+        bitmap = (unsigned short *) (base_frame_no * FRAME_SIZE);  // unsigned short * bitmap;
+    } else {
+        assert(_nframes * 2 <= FRAME_SIZE * 8 * _n_info_frames);
+        bitmap = (unsigned short *) (info_frame_no * FRAME_SIZE);
+    }
+
+    // Number of frames must be "fill" the bitmap!
+    assert ((nframes % (8 * 2)) == 0);
+
+    // Everything ok. Proceed to mark all bits in the bitmap to be FREE
+    memsetw(bitmap, 0xFFFF, _nframes / 8);
+    for (unsigned int i = 0; i < _nframes / 8; i++) {
+        assert(bitmap[i] == 0xFFFF);
+    }
+
+    // Mark the first frame as being used if it is being used
+    // For efficiency purpose, the first byte is used for head-of-sequence
+    // bit (0 means head), the second byte is used for free bit (0 means
+    // allocated, 1 means free), note that head-of-sequence's free bit will
+    // be cleared as 0 to indicate allocated.
+    if (_info_frame_no == 0) {
+        bitmap[0] = 0x7F7F;
+        n_free_frames--;
+    }
+
+    // link it in the list
+    if (list_head == NULL) {
+        list_head = this;
+    } else {
+        //WRITE THIS YOURSELF
+        ContFramePool* curr = ContFramePool::list_head;
+        while (curr->next_pool) {
+            curr = curr->next_pool;
+        }
+        curr->next_pool = this;
+    }
+    next_pool = NULL; // pointer to the next pool
+
+    Console::puts("Frame Pool initialized\n");
 }
 
 unsigned long ContFramePool::get_frames(unsigned int _n_frames)
 {
-    // TODO: IMPLEMENTATION NEEEDED!
-    assert(false);
+    // Any free frames left to allocate?
+    assert(n_free_frames > 0);
+
+    // If not enough frames, return 0
+    if (_n_frames > n_free_frames) {
+        Console::puts("Not enough free frames\n");
+        return 0;
+    }
+
+#ifdef DEBUG_FRAMEPOOL
+    Console::puts("Requesting ");
+    Console::puti(_n_frames);
+    Console::puts(" frames\n");
+#endif
+    unsigned long head_frame_no = 0;
+
+    unsigned short free_bit_mask = 0xFF;
+    unsigned short mask = 0x80;
+    unsigned int bitmap_index = 0;
+    unsigned int i = 0;
+    unsigned int offset = 0;
+    unsigned int new_offset = 0;
+    unsigned int n_cont_free_frames = 0;
+
+    // find the head of first free frame sequence (base_frame_no + bitmap_index * 8 + offset),
+    // the above check should guaranttee we can find a free frame
+    while ((bitmap[i] & free_bit_mask) == 0x0) {
+        i++;
+    }
+    bitmap_index = i;
+    while ((bitmap[i] & mask) == 0) {
+        mask >>= 1;
+        offset++;
+    }
+    while (i * 8 < nframes) {  // each index is used for 8 frames
+        while ((bitmap[i] & mask) != 0) {
+            mask >>= 1;
+            n_cont_free_frames++;
+        }
+
+        // check if find enough contiguous free frames
+        if (n_cont_free_frames >= _n_frames) {
+            break;
+        }
+
+        if (mask == 0) {
+            i++;
+            while (((bitmap[i] & free_bit_mask) == 0xFF) && (i * 8 < nframes)) {
+                i++;
+                n_cont_free_frames += 8;
+                // check if find enough contiguous free frames
+                if (n_cont_free_frames >= _n_frames) {
+                    break;
+                }
+            }
+            if (i * 8 >= nframes) {
+                return 0;
+            }
+            // check if find enough contiguous free frames or cross boundary
+            // WRITE THIS YOURSELF
+
+            // pass check, there is allocated frame(s) in this bitmap element
+            mask = 0x80;
+            while ((bitmap[i] & mask) != 0) {
+                mask >>= 1;
+                n_cont_free_frames++;
+            }
+
+            // check if find enough contiguous free frames
+            if (n_cont_free_frames >= _n_frames) {
+                break;
+            }
+        }
+
+        // find next head of free frame sequence
+        assert(mask != 0);
+        n_cont_free_frames = 0;
+        mask = 0x80;
+        while ((bitmap[i] & mask) != 0) {
+            mask >>= 1;
+            offset++;
+        }
+        assert(mask != 0);
+        while (((bitmap[i] & mask) == 0) && mask != 0) {
+            mask >>= 1;
+            offset++;
+        }
+
+        // check if can find the next head in this bitmap element
+        if (offset == 8) {
+            i++;
+            while (((bitmap[i] & free_bit_mask) == 0x0) && (i * 8 < nframes)) {
+                i++;
+            }
+            if (i * 8 >= nframes) {
+                break;
+            }
+            bitmap_index = i;
+            mask = 0x80;
+            offset = 0;
+            while ((bitmap[i] & mask) == 0) {
+                mask >>= 1;
+                offset++;
+            }
+        } else {
+            bitmap_index = i;
+        }
+    }
+
+    if (n_cont_free_frames >= _n_frames) {
+        unsigned int nbitmap_blocks = 1;
+        if ((_n_frames - (8 - offset)) > 0) {
+            nbitmap_blocks += (_n_frames - (8 - offset) + 8) / 8;
+        }
+        head_frame_no = base_frame_no + bitmap_index * 8 + offset;
+#ifdef DEBUG_FRAMEPOOL
+        Console::putui(_n_frames);
+        Console::puts(" consecutive frames found, head: ");
+        Console::puti(head_frame_no);
+        Console::puts(", base_frame_no: ");
+        Console::puti(base_frame_no);
+        Console::puts(", offset: ");
+        Console::puti(offset);
+        Console::puts(", bitmap: ");
+        for (i = bitmap_index; (i - bitmap_index) < nbitmap_blocks; i++) {
+            Console::putch('[');
+            Console::puti(i);
+            Console::puts("] ");
+            Console::putux(bitmap[i]);
+            Console::putch(' ');
+        }
+        Console::putch('\n');
+#endif
+        nframes -= _n_frames;
+        // Update bitmap
+        i = bitmap_index;
+        mask = 0x8000 >> offset;
+#ifdef DEBUG_FRAMEPOOL
+        Console::puts("Update head of sequence: [");
+        Console::puti(i);
+        Console::puts("]: ");
+        Console::putux(bitmap[i]);
+#endif
+        bitmap[i] ^= mask; // update the head bit
+#ifdef DEBUG_FRAMEPOOL
+        Console::puts(" -> [");
+        Console::puti(i);
+        Console::puts("]: ");
+        Console::putux(bitmap[i]);
+        Console::putch('\n');
+#endif
+        // MARK ALL THE OTHER N-1 FRAMES AS ALLOCATED
+
+        // WRITE THIS YOURSELF
+        mask = (0x80 >> offset);
+        // for loop for _n_frames
+        for (unsigned long counter = 0; counter <= _n_frames - 1; counter++) {
+            bitmap[i] ^= mask;
+            mask >>= 1;
+            if (mask == 0x00) {
+                mask = 0x80;
+                i++;
+            }
+        }
+#ifdef DEBUG_FRAMEPOOL
+        Console::puts("Updated bitmap: ");
+        for (i = bitmap_index; (i - bitmap_index) < nbitmap_blocks; i++) {
+            Console::putch('[');
+            Console::puti(i);
+            Console::puts("] ");
+            Console::putux(bitmap[i]);
+            Console::putch(' ');
+        }
+        Console::putch('\n');
+#endif
+    }
+
+    return head_frame_no;
 }
 
 void ContFramePool::mark_inaccessible(unsigned long _base_frame_no,
                                       unsigned long _n_frames)
 {
-    // TODO: IMPLEMENTATION NEEEDED!
-    assert(false);
+    unsigned int bitmap_index = (_base_frame_no - base_frame_no) / 8;
+    unsigned int offset = (_base_frame_no - base_frame_no) % 8;
+    // Update bitmap
+    unsigned int i = bitmap_index;
+    unsigned short mask = 0x8000 >> offset;
+    bitmap[i] ^= mask; // update the head bit
+    mask = 0x80 >> offset;
+    while (mask > 0 && _n_frames > 0) {
+        bitmap[i] ^= mask;
+        mask >>= 1;
+        _n_frames--;
+    }
+    i++;
+    while (_n_frames >= 8) {
+        bitmap[i] &= 0xFF00;
+        _n_frames -= 8;
+        i++;
+    }
+    mask = 0x80;
+    while (_n_frames > 0) {
+        bitmap[i] ^= mask;
+        mask >>= 1;
+        _n_frames--;
+    }
+    assert(mask != 0);
+}
+bool ContFramePool::check_and_release_frames(unsigned long _first_frame_no)
+{
+    unsigned long end_frame_no = base_frame_no + nframes;
+    if ((_first_frame_no < base_frame_no) || (_first_frame_no >= end_frame_no)) {
+        return false;
+    }
+
+    // Update bitmap
+    unsigned int bitmap_index = (_first_frame_no - base_frame_no) / 8;
+    unsigned int offset = (_first_frame_no - base_frame_no) % 8;
+
+    unsigned int i = bitmap_index;
+    unsigned short head_bit_mask = 0x8000 >> offset;
+    unsigned short free_bit_mask = 0x80 >> offset;
+
+    if ((bitmap[i] & head_bit_mask) != 0) {
+        Console::puts("ERROR: Frame to be released is not head of sequence\n");
+        return false;
+    }
+    bitmap[i] |= head_bit_mask; // update the head bit
+
+    head_bit_mask = 0x8000 >> offset;
+    while (((bitmap[i] & head_bit_mask) != 0) &&
+        ((bitmap[i] & free_bit_mask) == 0) &&
+        (free_bit_mask != 0)) {
+        bitmap[i] |= free_bit_mask;
+        head_bit_mask >>= 1;
+        free_bit_mask >>= 1;
+    }
+    if (free_bit_mask != 0) {
+        return true;
+    }
+
+    i++;
+    while ((bitmap[i] == 0xFF00) && (i * 8 < nframes)) {
+        bitmap[i] = 0xFFFF;
+        i++;
+    }
+
+    if (i * 8 >= nframes) {
+        return true;
+    }
+
+    head_bit_mask = 0x8000;
+    free_bit_mask = 0x80;
+    while (((bitmap[i] & head_bit_mask) != 0) && ((bitmap[i] & free_bit_mask) == 0)) {
+        // WRITE THIS YOURSELF
+        // this condition passes for allocated frames
+        // simply set the entire bitmap to 11111111 11111111
+        bitmap[i] = 0xFFFF;
+        i++;
+    }
+    assert(free_bit_mask != 0);
+    return true;
 }
 
 void ContFramePool::release_frames(unsigned long _first_frame_no)
 {
-    // TODO: IMPLEMENTATION NEEEDED!
-    assert(false);
-}
+    ContFramePool * frame_pool_ptr = list_head;
 
+    while (frame_pool_ptr != NULL) {
+        if (frame_pool_ptr->check_and_release_frames(_first_frame_no) == true) {
+            return;
+        }
+        // GO TO THE NEXT POOL
+        // WRITE THIS YOURSELF
+        frame_pool_ptr = frame_pool_ptr->next_pool;
+    }
+
+    Console::puts("ERROR: Frame not released, belong to no one or not allcated\n");
+    assert(0);
+}
 unsigned long ContFramePool::needed_info_frames(unsigned long _n_frames)
 {
-    // TODO: IMPLEMENTATION NEEEDED!
-    assert(false);
+    return (_n_frames / (16 KB * 8) + ((_n_frames % (16 KB * 8) == 0 ? 0 : 1)));  // Check if the calculation is correct
 }
--- MP4_Sources/page_table.C
+++ MP4_Sources/page_table.C
@@ -15,42 +15,123 @@ void PageTable::init_paging(ContFramePool * _kernel_mem_pool,
                             ContFramePool * _process_mem_pool,
                             const unsigned long _shared_size)
 {
-    assert(false);
-    Console::puts("Initialized Paging System\n");
+   PageTable::kernel_mem_pool = _kernel_mem_pool;
+   PageTable::process_mem_pool = _process_mem_pool;
+   PageTable::shared_size = _shared_size;
+   Console::puts("Initialized Paging System\n");
 }
 
 PageTable::PageTable()
 {
-    assert(false);
-    Console::puts("Constructed Page Table object\n");
+//initialize if page directory null
+   if (page_directory == NULL){
+      //Put the page_directory in kernel_mem_pool
+      page_directory = ( unsigned long*)(kernel_mem_pool->get_frames(1)*PAGE_SIZE); 
+      
+      //Direct map page_table
+      unsigned long *kernelPageTable = (unsigned long*)(kernel_mem_pool->get_frames(1)*PAGE_SIZE); 
+      unsigned long  addr = 0;
+
+      for (unsigned int i = 0; i < ENTRIES_PER_PAGE; i++){
+      kernelPageTable[i] = addr| 0x3;
+      addr += PAGE_SIZE;
+      }
+      page_directory[0] = (unsigned long) kernelPageTable| 0x3;
+   }
+   for(unsigned int i = 1; i < ENTRIES_PER_PAGE; i++)
+        page_directory[i] = 0 | 0x2; 
+    page_directory[ENTRIES_PER_PAGE - 1] = (unsigned long)page_directory | 0x3;
+
+    for (unsigned int i = 0; i < 16; ++i) {
+        VM_Pools[i] = NULL;
+    }
+    paging_enabled = false;
+    write_cr0(read_cr0() & 0x7FFFFFFF);
+  
+   Console::puts("Constructed Page Table object\n");
 }
 
 
 void PageTable::load()
 {
-    assert(false);
+    current_page_table = this;
+    write_cr3((unsigned long) this->page_directory);
     Console::puts("Loaded page table\n");
 }
 
 void PageTable::enable_paging()
 {
-    assert(false);
+    paging_enabled = 1;
+    write_cr0(read_cr0() | 0x80000000);
     Console::puts("Enabled paging\n");
 }
 
 void PageTable::handle_fault(REGS * _r)
 {
-    assert(false);
-    Console::puts("handled page fault\n");
+    unsigned long errorCode = _r->err_code;
+
+    if((errorCode & 0x1) == 1)
+        Console::puts("Protection Fault!\n");
+      
+    else { 
+        unsigned long *pageDir = (unsigned long *)read_cr3();      
+        unsigned long addr = read_cr2();
+
+        unsigned long *pageTable;
+
+        unsigned long page_tab_index = (addr / PAGE_SIZE) & 0x3FF;
+        unsigned long page_dir_index = addr / (ENTRIES_PER_PAGE*PAGE_SIZE);
+        VMPool** VMPoolList = current_page_table->VM_Pools;
+        bool legitimated = false;
+        for (unsigned int i = 0; i < 16; ++i) {
+            if((VMPoolList[i]!=NULL) && (VMPoolList[i]->is_legitimate(addr))) {
+                legitimated = true;
+                break;
+            }
+        }
+        if (legitimated) {
+            Console::puts("Address is not legitimated!!\n");
+        }
+        if(pageDir[page_dir_index] & 0x1 == 0x1) {  
+            pageTable = (unsigned long *) ((page_dir_index*PAGE_SIZE) | 0xFFC00000);
+        } 
+	    else {      
+            pageDir[page_dir_index] = (unsigned long)(process_mem_pool->get_frames(1)*PAGE_SIZE) | 0x7;
+            pageTable = (unsigned long *) ((page_dir_index*PAGE_SIZE) | 0xFFC00000);
+            for(unsigned int i = 0; i < ENTRIES_PER_PAGE; i++){
+                pageTable[i] = 0x6;
+            }
+	    }
+        pageTable[page_tab_index] = (PageTable::process_mem_pool->get_frames(1)*PAGE_SIZE) | 0x7;
+        Console::puts("Handled page fault\n");
+	}
 }
 
 void PageTable::register_pool(VMPool * _vm_pool)
 {
-    assert(false);
-    Console::puts("registered VM pool\n");
+	int VMPoolsIndex = -1;
+	for (unsigned int i = 0; i < 16; ++i) {
+		if (VM_Pools[i] == NULL) {
+			VMPoolsIndex = i;
+		}
+	}
+	if (VMPoolsIndex >= 0) {
+		VM_Pools[VMPoolsIndex] = _vm_pool;
+		    Console::puts("registered VM pool\n");
+	}
+    else {
+		Console::puts("ERROR: Failed to register new Virtual Memory Pool. Virtual Memory Pool array is full! \n");
+	}
+
 }
 
 void PageTable::free_page(unsigned long _page_no) {
-    assert(false);
+	unsigned long page_dir_index = _page_no / (ENTRIES_PER_PAGE*PAGE_SIZE);
+	unsigned long page_tab_index = (_page_no / PAGE_SIZE) & 0x3FF;
+	unsigned long* page_table = (unsigned long*)((page_dir_index * PAGE_SIZE) | 0xFFC00000);
+	unsigned long frame_number = page_table[page_tab_index];
+	process_mem_pool->release_frames(frame_number);
+	page_table[page_tab_index] = 0x0;
+	write_cr3(read_cr3());
     Console::puts("freed page\n");
 }
